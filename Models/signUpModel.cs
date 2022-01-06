861a2815e31a855c060d2f511648e0848784b834
Insha Bari <93077722+Inshabari@users.noreply.github.com>
2022-01-06 12:55:54 +0500

    commit 2

---
+++ Models/signUpModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace DigitalLibrary.Models
+{
+    public class signUpModel
+    {
+        public int ID { get; set; }
+
+        [Required]
+
+        public string name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string email { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string username { get; set; }
+
+        [Required]
+        [MinLength(6,ErrorMessage ="Too Short, Minimum Length should be 6 Characters")]
+        public string password { get; set; }
+
+        [Required]
+        public string confirm_pass { get; set; }
+
+        
+        public int isAdmin { get; set; }
+    }
+}
---
+++ Models/uploadBook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DigitalLibrary.Models
+{
+    public class uploadBook
+    {
+        public long BookID { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string bookName { get; set; }
+
+
+        [Required]
+        [MaxLength(40)]
+        public string writerName { get; set; }
+        public string bookCatagory { get; set; }
+
+        [Required]
+        public string publishDate { get; set; }
+
+        public int uploadedBy { get; set; }
+
+        public string uploadingDate { get; set; }
+
+        
+        public string bookPath { get; set; }
+
+        public string imagePath { get; set; }
+
+        [MaxLength(100)]
+        public string searchTag { get; set; }
+
+
+        [Required]
+        public HttpPostedFileBase ImageFile { get; set; }
+
+
+        [Required]
+        public HttpPostedFileBase pdfFile { get; set; }
+    }
+}
